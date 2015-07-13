4a7f204e67f1d26a80633d3d67bfe69a34ed45c1
Daniel Renteria <danielfelo@gmail.com>
2015-07-13 06:26:19 -0500

    Movil v0.1 Menores

--- gen/edu/uniandes/ecos/movilmigranha/R.java
+++ gen/edu/uniandes/ecos/movilmigranha/R.java
@@ -41,6 +41,7 @@ public final class R {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int login_button=0x7f050005;
         public static final int password_default=0x7f050004;
         public static final int username_default=0x7f050003;
     }
